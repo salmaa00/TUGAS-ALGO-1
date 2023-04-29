ec4310f82c982d167036eb141e4aad3b2a0518c6
salmaa00 <132159672+salmaa00@users.noreply.github.com>
2023-04-30 00:58:27 +0700

    Update Tugas11.java

--- Tugas11.java
+++ Tugas11.java
@@ -8,25 +8,26 @@ import java.util.Scanner;
 public class Tugas11 {
 
     public static void main(String[] args) {
-            Scanner masuk = new Scanner (System.in);
-            int jml;
-            String[] nama = new String[5];
-            Scanner scan = new Scanner (System.in);
-           double nilai,total = 0,rata;
+            Scanner masuk = new Scanner (System.in); //membuat Scanner baru dengan inputan baru
+            int jml; //mendeklarasikan variabel jumlah
+            String[] nama = new String[5]; //mendeklarasikan variabel array
+            Scanner scan = new Scanner (System.in); //membuat Scanner baru dengan inputan Scan
+           double nilai,total = 0,rata; //mendeklarasikan variabel nilai,total dan rata-rata
            
-           System.out.print ("Masukan jumlah mahasiswa = ");
-           jml = masuk.nextInt();
-                  
-           for (int i=1; i<=jml; i++){
-               System.out.print ("Masukan nama mahasiswa = ");
-               nama[i]= scan.nextLine();
-               System.out.print ("Masukan nilai mahasiswa "+ i + "= ");
-               nilai = masuk.nextDouble();
-               total += nilai;
+           System.out.print ("Masukan jumlah mahasiswa = "); //menampilkan output "Masukkan jumlah mahasiswa"
+           jml = masuk.nextInt(); //menyimpan inputan variabel jumlah
+           
+            // mengisi data mahasiswa dan nilai
+           for (int i=1; i<=jml; i++){ //untuk melakukan perulangan
+               System.out.print ("Masukan nama mahasiswa = "); //menampilkan output "Masukkan nama mahasiswa"
+               nama[i]= scan.nextLine(); //menyimpan inputan variabel nama menggunakan Scan
+               System.out.print ("Masukan nilai mahasiswa "+ i + "= "); //menampilkan output nilai mahasiswa"
+               nilai = masuk.nextDouble(); //menyimpan inputan variabel nilai menggunakan masuk
+               total += nilai; //menggunakan operator += untuk menambahkan total dengan nilai untuk menhasilkan total
                
            }
-           rata = total/jml;
-           System.out.println ("Total nilai mahasiswa = " +total);
-           System.out.println ("Rata-Rata nilai mahasiswa = " +rata);
+           rata = total/jml; //menggunakan operator / untuk menghasilkan rata-rata
+           System.out.println ("Total nilai mahasiswa = " +total); //menampilkan output "Total nilai mahasiswa"
+           System.out.println ("Rata-Rata nilai mahasiswa = " +rata); //menampilkan "Rata-rata nilai mahasiswa"
         }
 }
